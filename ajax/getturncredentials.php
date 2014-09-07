46ffef7e5443cb8986c96496874aaebcd906f801
Klaus Herberth <github@spam.herberth.eu>
2014-09-07 15:53:07 +0200

    fix TURN-REST-API

--- ajax/getturncredentials.php
+++ ajax/getturncredentials.php
@@ -6,10 +6,10 @@ $secret = OCP\Config::getAppValue ( 'ojsxc', 'iceSecret' );
 $user = OCP\User::getUser ();
 
 $data = array ();
-$data ['url'] = OCP\Config::getAppValue ( 'ojsxc', 'iceUrl' ); // $user.':'.
-$data ['username'] = OCP\Config::getAppValue ( 'ojsxc', 'iceUsername' ) ?  : ($secret ? time () : $user);
+$data ['ttl'] = OCP\Config::getAppValue ( 'ojsxc', 'iceTtl' ) ?  : 3600 * 24; // one day (according to TURN-REST-API)
+$data ['url'] = OCP\Config::getAppValue ( 'ojsxc', 'iceUrl' ); 
+$data ['username'] = OCP\Config::getAppValue ( 'ojsxc', 'iceUsername' ) ?  : ($secret ? (time () + $data ['ttl']) . ':' . $user : $user);
 $data ['credential'] = OCP\Config::getAppValue ( 'ojsxc', 'iceCredential' ) ?  : ($secret ? base64_encode ( hash_hmac ( 'sha1', $data ['username'], $secret, true ) ) : '');
-$data ['ttl'] = OCP\Config::getAppValue ( 'ojsxc', 'iceTtl' ) ?  : 60 * 60 * 12;
 
 OCP\JSON::encodedPrint ( $data );
 ?>
