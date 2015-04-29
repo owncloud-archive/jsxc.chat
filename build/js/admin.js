2641928b25c83728fc8dc8969e9601f687089210
Klaus Herberth <github@spam.herberth.eu>
2015-04-29 20:15:53 +0200

    build v2.0.0-beta2

--- build/js/admin.js
+++ build/js/admin.js
@@ -1,5 +1,5 @@
 /*!
- * ojsxc v2.0.0-beta1 - 2015-04-29
+ * ojsxc v2.0.0-beta2 - 2015-04-29
  * 
  * Copyright (c) 2015 Klaus Herberth <klaus@jsxc.org> <br>
  * Released under the MIT license
@@ -7,7 +7,7 @@
  * Please see http://www.jsxc.org/
  * 
  * @author Klaus Herberth <klaus@jsxc.org>
- * @version 2.0.0-beta1
+ * @version 2.0.0-beta2
  * @license MIT
  */
 
