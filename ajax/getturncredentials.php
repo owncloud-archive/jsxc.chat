7bd5e88a4bdb86793d7620c7f9b882e0e679bc7f
sualko <klaus@jsxc.org>
2015-08-05 10:44:49 +0200

    fix TURN-REST-API credential generation

--- ajax/getturncredentials.php
+++ ajax/getturncredentials.php
@@ -8,8 +8,8 @@ $user = OCP\User::getUser ();
 $ttl = OCP\Config::getAppValue ( 'ojsxc', 'iceTtl' ) ?  : 3600 * 24; // one day (according to TURN-REST-API)
 $url = OCP\Config::getAppValue ( 'ojsxc', 'iceUrl' );
 $url = $url ?  "turn:$url" : $url;
-$username = OCP\Config::getAppValue ( 'ojsxc', 'iceUsername' ) ?  : ($secret ? (time () + $data ['ttl']) . ':' . $user : $user);
-$credential = OCP\Config::getAppValue ( 'ojsxc', 'iceCredential' ) ?  : ($secret ? base64_encode ( hash_hmac ( 'sha1', $data ['username'], $secret, true ) ) : '');
+$username = OCP\Config::getAppValue ( 'ojsxc', 'iceUsername' ) ?  : ($secret ? (time () + $ttl) . ':' . $user : $user);
+$credential = OCP\Config::getAppValue ( 'ojsxc', 'iceCredential' ) ?  : ($secret ? base64_encode ( hash_hmac ( 'sha1', $username, $secret, true ) ) : '');
 
 $data = array (
    'ttl' => $ttl,
