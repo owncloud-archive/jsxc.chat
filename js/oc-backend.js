fbf480e4de8087d125ef251940504b0bd8d5a290
LEDfan <tobia@ledfan.be>
2015-09-06 09:41:27 +0200

    Remove old code

--- js/oc-backend.js
+++ js/oc-backend.js
@@ -1,30 +1,6 @@
 /**
  * This function is a hack. Please FIXME!
  */
-//jsxc.options.favicon.enable = false;
-//jsxc.options.otr.enable = false;
-//Strophe.getNodeFromJid = function () {
-//	return OC.currentUser;
-//};
-//Strophe.getDomainFromJid = function () {
-//	return '33.33';
-//};
-//
-//Strophe.getBareJidFromJid = function () {
-//	return OC.currentUser + '@33.3';
-//};
-//function ocInit(){
-//	localStorage.clear();
-//	jsxc.storage.setItem('debug', true);
-//	jsxc.options.set('xmpp', {'url': '/index.php/apps/ojsxc/http-bind'});
-//	jsxc.xmpp.login(OC.currentUser + '@33.3', 3434, 3443243); // TODO remove the hardcoded values
-//	jsxc.xmpp.connected();
-//	jsxc.storage.setItem('debug', true);
-//}
-//
-//$(document).ready(function () {
-//	ocInit();
-//});
 $(document).ready(function () {
 	localStorage.clear()
 	jsxc.options.set('favicon', {enable: false});
