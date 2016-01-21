cf3ce12d619cb411e27f553dae8f65c81a5b8149
LEDfan <tobia@ledfan.be>
2016-01-21 08:39:05 +0100

    Use jsxc.start to setup connection

--- js/oc-backend.js
+++ js/oc-backend.js
@@ -1,3 +1,5 @@
 /* global OC */
-
-jsxc.options.xmpp.url = OC.generateUrl('apps/ojsxc/http-bind');
+$(document).ready(function () {
+	jsxc.options.xmpp.url = OC.generateUrl('apps/ojsxc/http-bind');
+	jsxc.start(OC.currentUser + '@' + OC.getHost(), Math.floor(Math.random() * (1000 - 1)) + 1, Math.floor(Math.random() * (1000 - 1)) + 1);
+});
