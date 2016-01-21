5b0cb093668047242f653b0053e6c35239204190
LEDfan <tobia@ledfan.be>
2016-01-21 09:25:50 +0100

    Maybe this solves the bug

--- js/oc-backend.js
+++ js/oc-backend.js
@@ -2,4 +2,6 @@
 $(document).ready(function () {
 	jsxc.options.xmpp.url = OC.generateUrl('apps/ojsxc/http-bind');
 	jsxc.start(OC.currentUser + '@' + OC.getHost(), Math.floor(Math.random() * (1000 - 1)) + 1, Math.floor(Math.random() * (1000 - 1)) + 1);
+	jsxc.xmpp.conn.resume();
+	jsxc.onMaster();
 });
